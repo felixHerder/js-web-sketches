d2f26f3c259b240cadba0970a0e8228af69089ce
felixHerder <felixbaciu@gmail.com>
2021-05-27 12:34:42 +0300

    link pad

--- link-pad/script.js
+++ link-pad/script.js
@@ -6,22 +6,24 @@ const webNameEl = document.getElementById('website-name');
 const webUrlEl = document.getElementById('website-url');
 const linkContainer = document.getElementById('links-container');
 const submitBtn = document.getElementById('submit-button');
-const spinner = document.querySelector('.fa-spinner')
+const spinner = document.querySelector('.fa-spinner');
+
+let links = [];
 
 //Show modal, focus on input
 function showModal() {
     spinner.style.display = 'none';
     modal.classList.add('show-modal');
     webNameEl.parentElement.hidden = true;
-    webNameEl.value = '';
-    webUrlEl.value = '';
+    linkForm.reset();
     submitBtn.disabled = true;
     webUrlEl.focus();
 }
 
 modalShow.addEventListener('click', showModal);
-modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
-window.addEventListener('click', (e) => e.target === modal ? modal.classList.remove('show-modal') : false);
+//hide modal
+modalClose.addEventListener('mousedown', () => modal.classList.remove('show-modal'));
+window.addEventListener('mousedown', (e) => e.target === modal ? modal.classList.remove('show-modal') : false);
 
 //auto populate website name
 const proxyUrl = 'https://cors.bridged.cc/';
@@ -60,6 +62,63 @@ webUrlEl.addEventListener('change', async() => {
     }
 });
 
+//create item card
+function createItem(webname, weburl) {
+    const item = document.createElement('div');
+    item.classList.add('item');
+    item.setAttribute('weburl', weburl);
+    linkContainer.appendChild(item);
+
+    const closebtn = document.createElement('i');
+    closebtn.classList.value = ('far fa-window-close');
+    closebtn.tagName = 'delete-link';
+    closebtn.title = 'delete link';
+    //remove item 
+    closebtn.addEventListener('click', (e) => {
+        links = links.filter(elem => elem.url != weburl);
+        e.target.parentElement.remove();
+        localStorage.setItem('links', JSON.stringify(links));
+    });
+    item.appendChild(closebtn);
+
+    const name = document.createElement('div');
+    name.classList.add('name');
+    item.appendChild(name);
+
+    const img = document.createElement('img');
+    img.src = "https://www.google.com/s2/favicons?domain=" + weburl;
+    img.alt = 'favicon';
+    name.appendChild(img);
+
+    const link = document.createElement('a');
+    link.href = weburl;
+    link.title = webname;
+    link.textContent = webname;
+    link.target = "_blank";
+    name.appendChild(link);
+}
+
+//load links from local storage
+function loadLinks() {
+    if (localStorage.getItem('links')) {
+        links = JSON.parse(localStorage.getItem('links'));
+    } else {
+        //default links
+        links = [{ name: 'Unsplash', url: 'https://unsplash.com' },
+            { name: 'reddit: the front page of the internet', url: 'https://reddit.com' },
+            { name: 'DuckDuckGo — Privacy, simplified.', url: 'https://duckduckgo.com' },
+            { name: 'IMDb: Ratings, Reviews, and Where to Watch the Best Movies & TV Shows', url: 'https://imdb.com' }
+        ];
+
+        localStorage.setItem('links', JSON.stringify(links));
+    }
+    //poulate  iteams from links
+    links.forEach(element => {
+        createItem(element.name, element.url);
+    });
+}
+
+//store links
 function storeLink(e) {
     e.preventDefault();
     const nameValue = webNameEl.value;
@@ -67,41 +126,20 @@ function storeLink(e) {
     if (!urlValue.includes('http://', 'https://')) {
         urlValue = `https://${urlValue}`;
     }
+    const link = {
+        name: nameValue,
+        url: urlValue
+    }
+    links.push(link);
+    localStorage.setItem('links', JSON.stringify(links));
+    createItem(nameValue, urlValue);
+    //reset form
+    // webNameEl.parentElement.hidden = true;
+    // linkForm.reset();
+    // submitBtn.disabled = true;
+    // webUrlEl.focus();
+    modal.classList.remove('show-modal');
 }
 
-
+loadLinks();
 linkForm.addEventListener('submit', storeLink);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// fetchTitle('wikipedia.com').then(console.log);
-
-// const weburl = document.getElementById('website-url');
-// const webname = document.getElementById('website-name');
-
-// weburl.addEventListener('focusout', (e) => {
-//     fetchTitle(e.target.value).then(t => { webname.value = t });
-// });
